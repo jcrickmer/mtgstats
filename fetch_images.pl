6523ca460327d59d17c680353d1c6091b71d6b55
Jason Crickmer <jcrickmer@gmail.com>
2014-11-30 08:32:25 -0600

    Removed dependencies on MTG library so that I could push this out to card.ninja production without needing a whole bunch of other libraries.

--- fetch_images.pl
+++ fetch_images.pl
@@ -1,8 +1,7 @@
 #!/usr/bin/perl
 
 use strict;
-use MTG::Database;
-use MTG::GathererLoader;
+
 use URI::Escape;
 use Time::HiRes qw( usleep ualarm gettimeofday tv_interval nanosleep
                     clock_gettime clock_getres clock_nanosleep clock
@@ -23,12 +22,13 @@ foreach my $id (@multiverseids) {
     my $rurl = 'http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid=' . $id;
     my $cmd = 'curl -s -L -A ' . "'" . $agent . "' --referer '" . $rurl . "' '" . $iurl . "' > card_images/" . $id . '.jpg';
     if (! -e 'card_images/' . $id . '.jpg' || -s 'card_images/' . $id . '.jpg' < 100) {
-	printf("%d (%03.2f%% - %d of %d)\n", $id, 100.0 * $stopper / (1.0 * scalar(@multiverseids)), $stopper, scalar(@multiverseids));
-	#print "$cmd\n";
-	`$cmd`;
-	usleep(1000000 + (rand() * 3000000));
+		printf("%d (%03.2f%% - %d of %d)\n", $id, 100.0 * $stopper / (1.0 * scalar(@multiverseids)), $stopper, scalar(@multiverseids));
+		#print "$cmd\n";
+		`$cmd`;
+		#usleep(7000000 + (rand() * 5000000)); # 7-12 seconds, nice and slow
+		usleep(100000 + (rand() * 500000)); # fast!
     } else {
-	print "skipping $id - I already have it.\n";
+		print "skipping $id - I already have it.\n";
     }
     $stopper++;
     #last if ($stopper > 10);
